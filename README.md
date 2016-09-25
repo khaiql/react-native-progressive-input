d37913cc9cd67d9b3cb523268adef43af0995dcc
Scott Le <khai.le@live.com>
2016-09-25 20:40:40 +0700

    restructure

--- README.md
+++ README.md
@@ -1,47 +1,9 @@
 
-# react-native-progress-text-input
+# ProgressiveInput
 
 ## Getting started
 
 `$ npm install react-native-progress-text-input --save`
-
-### Mostly automatic installation
-
-`$ react-native link react-native-progress-text-input`
-
-### Manual installation
-
-
-#### iOS
-
-1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
-2. Go to `node_modules` ➜ `react-native-progress-text-input` and add `RNProgressTextInput.xcodeproj`
-3. In XCode, in the project navigator, select your project. Add `libRNProgressTextInput.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
-4. Run your project (`Cmd+R`)<
-
-#### Android
-
-1. Open up `android/app/src/main/java/[...]/MainActivity.java`
-  - Add `import com.reactlibrary.RNProgressTextInputPackage;` to the imports at the top of the file
-  - Add `new RNProgressTextInputPackage()` to the list returned by the `getPackages()` method
-2. Append the following lines to `android/settings.gradle`:
-  	```
-  	include ':react-native-progress-text-input'
-  	project(':react-native-progress-text-input').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-progress-text-input/android')
-  	```
-3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
-  	```
-      compile project(':react-native-progress-text-input')
-  	```
-
-#### Windows
-[Read it! :D](https://github.com/ReactWindows/react-native)
-
-1. In Visual Studio add the `RNProgressTextInput.sln` in `node_modules/react-native-progress-text-input/windows/RNProgressTextInput.sln` folder to their solution, reference from their app.
-2. Open up your `MainPage.cs` app
-  - Add `using Cl.Json.RNProgressTextInput;` to the usings at the top of the file
-  - Add `new RNProgressTextInputPackage()` to the `List<IReactPackage>` returned by the `Packages` method
-
 
 ## Usage
 ```javascript
@@ -50,4 +12,4 @@ import RNProgressTextInput from 'react-native-progress-text-input';
 // TODO: What do with the module?
 RNProgressTextInput;
 ```
-  
+
--- index.js
+++ index.js
@@ -1,1 +1,175 @@
-import ProgressiveInput from './lib/ProgressiveInput';
+import React, { Component, PropTypes } from 'react';
+import {
+  TextInput,
+  View,
+  ActivityIndicator,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+  },
+  clearIcon: {
+    marginLeft: 5
+  },
+  textInput: {
+    flex: 1,
+    height: 40,
+    marginLeft: 10
+  },
+  activityIndicator: {
+    marginLeft: 5,
+    marginRight: 5
+  }
+});
+
+class ProgressiveInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showClearButton: false,
+      value: this.props.value,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({value: nextProps.value});
+  }
+
+  clearInput() {
+    this.setState({value: '', focus: false});
+    if (this.props.onInputCleared) {
+      this.props.onInputCleared();
+    }
+  }
+
+  isFocused() {
+    return this.input.isFocused();
+  }
+
+  render() {
+    return (
+      <View style={[styles.container, this.props.style]}>
+        {this._renderClearButton()}
+        <TextInput
+          ref={(input) => this.input = input}
+          style={[styles.textInput, this.props.textInputStyle]}
+          focus={this.state.focus}
+          value={this.state.value}
+          editable={this.props.editable}
+          onFocus={this._onFocus.bind(this)}
+          placeholder={this.props.placeholder}
+          onChangeText={this._onChangeText.bind(this)}
+          selectTextOnFocus={this.props.selectTextOnFocus}
+          onBlur={this._onBlur.bind(this)}
+          autoCorrect={this.props.autoCorrect}
+          keyboardType={this.props.keyboardType}
+          multiline={this.props.multiline}
+          placeholderTextColor={this.props.placeholderTextColor}
+          returnKeyType={this.props.returnKeyType}
+          selectTextOnFocus={this.props.selectTextOnFocus}
+          autoCapitalize={this.props.autoCapitalize}
+          maxLength={this.props.maxLength}
+          multiline={this.props.multiline}
+          onEndEditing={this.props.onEndEditing}
+          onChange={this.props.onChange}
+        />
+        {this._renderActivityIndicator()}
+      </View>
+    );
+  }
+
+  _renderActivityIndicator() {
+    if (this.props.isLoading) {
+      return (
+        <ActivityIndicator
+          animating={this.props.isLoading}
+          style={[styles.activityIndicator, this.props.activityIndicatorStyle]}
+        />
+      );
+    }
+  }
+
+  _renderClearButton() {
+    if (this.state.showClearButton) {
+      return (
+        <TouchableOpacity onPress={() => this.clearInput()}>
+          <Icon name={this.props.clearButtonIcon} size={this.props.clearButtonSize} style={[ styles.clearIcon, this.props.clearButtonStyle ]} color={this.props.clearButtonColor} />
+        </TouchableOpacity>
+      );
+    }
+  }
+
+  _onFocus() {
+    this._shouldShowClearButton();
+    if (this.props.onFocus) {
+      this.props.onFocus();
+    }
+  }
+
+  _onChangeText(text) {
+    this.setState({value: text});
+    this._shouldShowClearButton(text);
+    if (this.props.onChangeText) {
+      this.props.onChangeText(text);
+    }
+  }
+
+  _shouldShowClearButton(value) {
+    let v = value || this.state.value;
+    let showClearButton = v ? true : false;
+    this.setState({showClearButton});
+  }
+
+  _onBlur() {
+    this.setState({showClearButton: false});
+  }
+}
+
+let textInputProps = {
+  autoCorrect,
+  keyboardType,
+  multiline,
+  placeholderTextColor,
+  returnKeyType,
+  selectTextOnFocus,
+  placeholder,
+  editable,
+  autoCapitalize,
+  maxLength,
+  multiline,
+  onEndEditing,
+  onChange,
+} = TextInput.propTypes;
+
+ProgressiveInput.propTypes = {
+  ...textInputProps,
+  value: PropTypes.string,
+  isLoading: PropTypes.bool,
+  textInputStyle: TextInput.propTypes.style,
+  clearButtonIcon: PropTypes.string,
+  clearButtonColor: PropTypes.string,
+  clearButtonSize: PropTypes.number,
+  clearButtonStyle: PropTypes.object,
+  activityIndicatorStyle: ActivityIndicator.propTypes.style,
+  onBlur: PropTypes.func,
+  onChangeText: PropTypes.func,
+  onFocus: PropTypes.func,
+  onInputCleared: PropTypes.func
+};
+
+ProgressiveInput.defaultProps = {
+  editable: true,
+  clearButtonIcon: 'times-circle',
+  clearButtonColor: 'lightgrey',
+  clearButtonSize: 20,
+};
+
+export default ProgressiveInput;
