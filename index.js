b04374231db123801c3016d2ddfc01f53a494df1
Evan Bacon <baconbrix@gmail.com>
2017-12-31 03:59:01 -0600

    @evanbacon/text props (#14)
    * Updated PropTypes
    
    * Updated example to Expo v23
    
    * Replaced `"` with `'`
    
    * Updated format
    
    * version revert - no functional changes 🤷‍♀️
    
    * fixed text props
    
    * updated icon
    
    * updated close icon

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; // 15.6.0
 import {
   TextInput,
   View,
@@ -7,31 +7,34 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: 'lightgrey',
-    backgroundColor: 'white',
-  },
-  clearIcon: {
-    marginLeft: 5,
-  },
-  textInput: {
-    flex: 1,
-    height: 40,
-    marginLeft: 10,
-  },
-  activityIndicator: {
-    marginLeft: 5,
-    marginRight: 5,
-  },
-});
+import { Ionicons } from '@expo/vector-icons'; // 6.2.2
 
 class ProgressiveInput extends Component {
+  static propTypes = {
+    ...TextInput.propTypes,
+    value: PropTypes.string,
+    isLoading: PropTypes.bool,
+    textInputStyle: TextInput.propTypes.style,
+    clearButtonIcon: PropTypes.string,
+    clearButtonColor: PropTypes.string,
+    clearButtonSize: PropTypes.number,
+    clearButtonStyle: PropTypes.object,
+    activityIndicatorStyle: ActivityIndicator.propTypes.style,
+    onBlur: PropTypes.func,
+    onChangeText: PropTypes.func,
+    onFocus: PropTypes.func,
+    onInputCleared: PropTypes.func,
+    underlineColorAndroid: PropTypes.string,
+  };
+
+  static defaultProps = {
+    editable: true,
+    clearButtonIcon: 'ios-close-circle',
+    clearButtonColor: 'lightgrey',
+    clearButtonSize: 20,
+    underlineColorAndroid: 'transparent',
+  };
+
   constructor(props) {
     super(props);
 
@@ -66,20 +69,18 @@ class ProgressiveInput extends Component {
           focus={this.state.focus}
           value={this.state.value}
           editable={this.props.editable}
-          onFocus={this._onFocus.bind(this)}
+          onFocus={this._onFocus}
           placeholder={this.props.placeholder}
-          onChangeText={this._onChangeText.bind(this)}
+          onChangeText={this._onChangeText}
           selectTextOnFocus={this.props.selectTextOnFocus}
-          onBlur={this._onBlur.bind(this)}
+          onBlur={this._onBlur}
           autoCorrect={this.props.autoCorrect}
           keyboardType={this.props.keyboardType}
           multiline={this.props.multiline}
           placeholderTextColor={this.props.placeholderTextColor}
           returnKeyType={this.props.returnKeyType}
-          selectTextOnFocus={this.props.selectTextOnFocus}
           autoCapitalize={this.props.autoCapitalize}
           maxLength={this.props.maxLength}
-          multiline={this.props.multiline}
           onEndEditing={this.props.onEndEditing}
           onChange={this.props.onChange}
           underlineColorAndroid={this.props.underlineColorAndroid}
@@ -89,7 +90,7 @@ class ProgressiveInput extends Component {
     );
   }
 
-  _renderActivityIndicator() {
+  _renderActivityIndicator = () => {
     let size = this.props.isLoading ? {} : { width: 0, height: 0 };
     return (
       <ActivityIndicator
@@ -101,13 +102,13 @@ class ProgressiveInput extends Component {
         ]}
       />
     );
-  }
+  };
 
-  _renderClearButton() {
+  _renderClearButton = () => {
     if (this.state.showClearButton) {
       return (
         <TouchableOpacity onPress={() => this.clearInput()}>
-          <Icon
+          <Ionicons
             name={this.props.clearButtonIcon}
             size={this.props.clearButtonSize}
             style={[styles.clearIcon, this.props.clearButtonStyle]}
@@ -116,73 +117,52 @@ class ProgressiveInput extends Component {
         </TouchableOpacity>
       );
     }
-  }
+  };
 
-  _onFocus() {
+  _onFocus = () => {
     this._shouldShowClearButton();
     if (this.props.onFocus) {
       this.props.onFocus();
     }
-  }
+  };
 
-  _onChangeText(text) {
+  _onChangeText = text => {
     this.setState({ value: text });
     this._shouldShowClearButton(text);
     if (this.props.onChangeText) {
       this.props.onChangeText(text);
     }
-  }
+  };
 
-  _shouldShowClearButton(value) {
+  _shouldShowClearButton = value => {
     const v = value || this.state.value;
     const showClearButton = v ? true : false;
     this.setState({ showClearButton });
-  }
+  };
 
-  _onBlur() {
-    this.setState({ showClearButton: false });
-  }
+  _onBlur = () => this.setState({ showClearButton: false });
 }
 
-const textInputProps = ({
-  autoCorrect,
-  keyboardType,
-  multiline,
-  placeholderTextColor,
-  returnKeyType,
-  selectTextOnFocus,
-  placeholder,
-  editable,
-  autoCapitalize,
-  maxLength,
-  multiline,
-  onEndEditing,
-  onChange,
-} = TextInput.propTypes);
-
-ProgressiveInput.propTypes = {
-  ...textInputProps,
-  value: PropTypes.string,
-  isLoading: PropTypes.bool,
-  textInputStyle: TextInput.propTypes.style,
-  clearButtonIcon: PropTypes.string,
-  clearButtonColor: PropTypes.string,
-  clearButtonSize: PropTypes.number,
-  clearButtonStyle: PropTypes.object,
-  activityIndicatorStyle: ActivityIndicator.propTypes.style,
-  onBlur: PropTypes.func,
-  onChangeText: PropTypes.func,
-  onFocus: PropTypes.func,
-  onInputCleared: PropTypes.func,
-  underlineColorAndroid: PropTypes.string,
-};
-
-ProgressiveInput.defaultProps = {
-  editable: true,
-  clearButtonIcon: 'times-circle',
-  clearButtonColor: 'lightgrey',
-  clearButtonSize: 20,
-  underlineColorAndroid: 'transparent',
-};
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    backgroundColor: 'white',
+  },
+  clearIcon: {
+    marginLeft: 5,
+  },
+  textInput: {
+    flex: 1,
+    height: 40,
+    marginLeft: 10,
+  },
+  activityIndicator: {
+    marginLeft: 5,
+    marginRight: 5,
+  },
+});
 
 export default ProgressiveInput;
